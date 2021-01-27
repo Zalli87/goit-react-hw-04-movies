557741f98d36860d39e634ae7b7619ab807d7677
Kostiantyn Harbuz <konst.garbuz@gmail.com>
2021-01-27 20:51:52 +0200

    добавляет правки

--- src/views/CastPage/CastPage.js
+++ src/views/CastPage/CastPage.js
@@ -1,28 +1,60 @@
+import PropTypes from 'prop-types';
 import { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import Loader from 'react-loader-spinner';
 import * as moviesAPI from '..//../serveses/movies-api';
+import notFoundImg from '..//../images/notFoundImg.jpg';
 
 export default function CastPage({ movieId }) {
   const BASE_URL = 'https://image.tmdb.org/t/p/w500';
   const [cast, setCast] = useState(null);
+  const [status, setStatus] = useState('idle');
+  const [error, setError] = useState(null);
 
-  console.log(movieId);
   useEffect(() => {
-    moviesAPI.fetchMovieCast(movieId).then(({ cast }) => setCast(cast));
-  }, []);
+    setStatus('pending');
+    moviesAPI
+      .fetchMovieCast(movieId)
+      .then(({ cast }) => {
+        if (cast.length === 0) {
+          setStatus('idle');
+          return;
+        }
+        setCast(cast);
+        setStatus('resolved');
+      })
+      .catch(error => {
+        setStatus('rejected');
+        setError(error);
+      });
+  }, [movieId]);
 
   return (
     <>
-      <ul>
-        {cast &&
-          cast.map(({ id, name, profile_path, character }) => (
-            <li>
-              <img src={`${BASE_URL}${profile_path}`} />
-              <p>{name}</p>
-              <p>Character: {character}</p>
-            </li>
-          ))}
-      </ul>
+      {status === 'idle' && <p>Not any cast on this movie</p>}
+      {status === 'pending' && <Loader />}
+      {status === 'resolved' && (
+        <>
+          <ul>
+            {cast.map(({ id, name, profile_path, character }) => (
+              <li key={id}>
+                <img
+                  src={
+                    profile_path ? `${BASE_URL}${profile_path}` : notFoundImg
+                  }
+                  alt={name}
+                />
+                <p>{name}</p>
+                <p>Character: {character}</p>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
+      {status === 'rejected' && <p>{error}</p>}
     </>
   );
 }
+
+CastPage.protoType = {
+  movieId: PropTypes.string.isRequired,
+};
--- src/views/ReviewsPage/ReviewsPage.js
+++ src/views/ReviewsPage/ReviewsPage.js
@@ -1,28 +1,53 @@
+import PropTypes from 'prop-types';
 import { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import Loader from 'react-loader-spinner';
 import * as moviesAPI from '..//../serveses/movies-api';
 
 export default function ReviewPage({ movieId }) {
   const [reviews, setReviews] = useState(null);
+  const [status, setStatus] = useState('idle');
+  const [error, setError] = useState(null);
 
-  console.log(movieId);
   useEffect(() => {
+    setStatus('pending');
+
     moviesAPI
       .fetchMovieReview(movieId)
-      .then(({ results }) => setReviews(results));
-  }, []);
+      .then(({ results }) => {
+        if (results.length === 0) {
+          setStatus('idle');
+          return;
+        }
+        setReviews(results);
+        setStatus('resolved');
+      })
+      .catch(error => {
+        setStatus('rejected');
+        setError(error);
+      });
+  }, [movieId]);
 
   return (
     <>
-      {reviews &&
-        reviews.map(({ author, content }) => (
-          <ul>
-            <li>
-              <h2>Author: {author}</h2>
-              <p>{content}</p>
-            </li>
-          </ul>
-        ))}
+      {status === 'idle' && <p>Not reviews for this movie</p>}
+      {status === 'pending' && <Loader />}
+      {status === 'resolved' && (
+        <>
+          {reviews.map(({ author, content }) => (
+            <ul key={author}>
+              <li>
+                <h2>Author: {author}</h2>
+                <p>{content}</p>
+              </li>
+            </ul>
+          ))}
+        </>
+      )}
+      {status === 'rejected' && <p>{error}</p>}
     </>
   );
 }
+
+ReviewPage.protoType = {
+  movieId: PropTypes.string.isRequired,
+};
